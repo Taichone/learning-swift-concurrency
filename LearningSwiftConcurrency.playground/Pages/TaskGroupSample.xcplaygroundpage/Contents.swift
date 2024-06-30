1333d322b027bdad4cb3d34f81a890275f6143fc
Taichone <taichone.dev@gmail.com>
2024-06-30 15:46:49 +0900

    add: learningTaskGroupResultForAwaitIn()

--- LearningSwiftConcurrency.playground/Pages/TaskGroupSample.xcplaygroundpage/Contents.swift
+++ LearningSwiftConcurrency.playground/Pages/TaskGroupSample.xcplaygroundpage/Contents.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+/// TaskGroup の Task は並列的に全て開始されること
 func performTasks() async -> Int {
     await withTaskGroup(of: Int.self) { group in
         group.addTask {
@@ -13,14 +14,14 @@ func performTasks() async -> Int {
                             print("Level 3 - Task 1.1.1 started and sleeping \(sleepNanoseconds)")
                             try? await Task.sleep(nanoseconds: sleepNanoseconds)
                             print("Level 3 - Task 1.1.1 completed")
-                            return 10
+                            return Int(sleepNanoseconds)
                         }
                         grandchildGroup.addTask {
                             let sleepNanoseconds: UInt64 = 3000000
                             print("Level 3 - Task 1.1.2 started and sleeping \(sleepNanoseconds)")
                             try? await Task.sleep(nanoseconds: sleepNanoseconds)
                             print("Level 3 - Task 1.1.2 completed")
-                            return 20
+                            return Int(sleepNanoseconds)
                         }
 
                         var sum = 0
@@ -37,7 +38,7 @@ func performTasks() async -> Int {
                     print("Level 2 - Task 1.2 started and sleeping \(sleepNanoseconds)")
                     try? await Task.sleep(nanoseconds: sleepNanoseconds)
                     print("Level 2 - Task 1.2 completed")
-                    return 30
+                    return Int(sleepNanoseconds)
                 }
 
                 var sum = 0
@@ -55,7 +56,7 @@ func performTasks() async -> Int {
             print("Level 1 - Task 2 started and sleeping \(sleepNanoseconds)")
             try? await Task.sleep(nanoseconds: sleepNanoseconds)
             print("Level 1 - Task 2 completed")
-            return 40
+            return Int(sleepNanoseconds)
         }
 
         var total = 0
@@ -67,7 +68,87 @@ func performTasks() async -> Int {
     }
 }
 
+/// .next() メソッドでは、addTask の順に関係なく、return された順に値が返されることを確認
+func learningTaskGroupResultNext() async -> String {
+    await withTaskGroup(of: Int.self) { group in
+        group.addTask {
+            let time = 5000000000
+            try? await Task.sleep(nanoseconds: UInt64(time))
+            print("return \(time)")
+            return time
+        }
+        group.addTask {
+            let time = 1000000000
+            try? await Task.sleep(nanoseconds: UInt64(time))
+            print("return \(time)")
+            return time
+        }
+        group.addTask {
+            let time = 3000000000
+            try? await Task.sleep(nanoseconds: UInt64(time))
+            print("return \(time)")
+            return time
+        }
+
+        print("gettingFirstResult")
+        guard let firstResult = await group.next() else {
+            group.cancelAll()
+            return "error"
+        }
+
+        print("gettingSecondResult")
+        guard let secondResult = await group.next() else {
+            group.cancelAll()
+            return "error"
+        }
+
+        print("gettingThirdResult")
+        guard let thirdResult = await group.next() else {
+            group.cancelAll()
+            return "error"
+        }
+
+        print(firstResult)
+        print(secondResult)
+        print(thirdResult)
+
+        return "success"
+    }
+}
+
+/// for await in メソッドでは、addTask の順に関係なく、return された順に値が返されることを確認
+func learningTaskGroupResultForAwaitIn() async -> String {
+    await withTaskGroup(of: Int.self) { group in
+        group.addTask {
+            let time = 5000000000
+            try? await Task.sleep(nanoseconds: UInt64(time))
+            print("return \(time)")
+            return time
+        }
+        group.addTask {
+            let time = 1000000000
+            try? await Task.sleep(nanoseconds: UInt64(time))
+            print("return \(time)")
+            return time
+        }
+        group.addTask {
+            let time = 3000000000
+            try? await Task.sleep(nanoseconds: UInt64(time))
+            print("return \(time)")
+            return time
+        }
+
+        print("for await in 開始")
+        for await taskResult in group {
+            print(taskResult)
+        }
+
+        return "success"
+    }
+}
+
 Task {
-    let result = await performTasks()
-    print("Final result: \(result)")
+    await print("performTasks() result: \(performTasks())")
+    await print("learningTaskGroupResultNext() result: \(learningTaskGroupResultNext())")
+    await print("learningTaskGroupResultForAwaitIn() result: \(learningTaskGroupResultForAwaitIn())")
 }
